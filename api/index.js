b5be939fcae6aae283ba0f51e5eede91a35763b3
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-02-02 16:46:21 +0100

    remove duplicate legacy

--- api/index.js
+++ api/index.js
@@ -1,36 +1,31 @@
 // Vars
-const dotenv = require("dotenv");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const dotenv = require('dotenv')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Express App
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
 // Select the env file
-dotenv.config();
-const PORT = process.env.API_PORT || 5000;
+dotenv.config()
+const PORT = process.env.API_PORT || 5000
 
 // Models
-require("./src/sync")();
+require('./src/sync')()
 
 // Swagger
-require("./src/swagger")(app);
-
+require('./src/swagger')(app)
 
 // Routes
 //require("./src/routes")(app);
-require("./src/routes/user.routes")(app);
-require("./src/routes/trip.routes")(app);
-require("./src/routes/auth.routes")(app);
-require("./src/routes/car.routes")(app);
-require("./src/routes/segment.routes")(app);
-require("./src/routes/request.routes")(app);
+require('./src/routes/user.routes')(app)
+require('./src/routes/auth.routes')(app)
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`[EXPRESS] - Server listening on port ${PORT}`);
-});
+  console.log(`[EXPRESS] - Server listening on port ${PORT}`)
+})
