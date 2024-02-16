56d4758e71b60a09e05b90fb6a8cf802519509a8
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-02-16 15:20:33 +0100

    add unit testing to app

---
+++ api/test/auth/auth.test.js
@@ -0,0 +1,75 @@
+const app = require('../index')
+const supertest = require('supertest')
+
+let chai
+let expect
+
+import('chai').then((Chai) => {
+  chai = Chai
+  expect = chai.expect
+})
+
+const request = supertest(app)
+
+describe('POST /api/auth/register', function () {
+  it('creates a user', function (done) {
+    const user = {
+      email: 'test@test.com',
+      password: 'testtest12',
+      password2: 'testtest12',
+      firstname: 'John',
+      lastname: 'Doe',
+      birthdate: '2000-12-12',
+    }
+
+    request
+      .post('/api/auth/register')
+      .send(user)
+      .end(function (err, res) {
+        expect(res.status).to.equal(201)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('email')
+        expect(res.body).to.have.property('firstname')
+        done(err)
+      })
+  })
+
+  it('fails to create a user', function (done) {
+    const user = {
+      email: 'johndoe@gmail.com',
+      password: 'testtest12',
+      password2: 'testtest',
+      firstname: 'John',
+      lastname: 'Doe',
+      birthdate: '2000-12-12',
+    }
+
+    request
+      .post('/api/auth/register')
+      .send(user)
+      .end(function (err, res) {
+        expect(res.status).to.equal(400)
+        expect(res.body).to.have.property('errors').to.be.an('array')
+        expect(res.body.errors[0])
+          .to.have.property('msg')
+          .to.equal('Passwords must match')
+        done(err)
+      })
+  })
+
+  it('authenticate a user', function (done) {
+    const payload = {
+      email: 'test@test.com',
+      password: 'testtest12',
+    }
+
+    request
+      .post('/api/auth/login')
+      .send(payload)
+      .end(function (err, res) {
+        expect(res.status).to.equal(200)
+        expect(res.body).to.have.property('token')
+        done(err)
+      })
+  })
+})
---
+++ api/test/index.js
@@ -0,0 +1,33 @@
+// Vars
+const dotenv = require('dotenv')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Express App
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// Select the env file
+dotenv.config()
+const PORT = 5001
+
+// Models
+require('../src/sync')()
+
+// Swagger
+require('../src/swagger')(app)
+
+// Routes
+//require("./src/routes")(app);
+require('../src/routes/user.routes')(app)
+require('../src/routes/auth.routes')(app)
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`[EXPRESS] - Server listening on port ${PORT}`)
+})
+
+module.exports = app
