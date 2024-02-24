7466db75f792c08e97e38cba7cee5785e61afa33
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-02-24 15:48:24 +0100

    run test outside container

--- api/src/database.js
+++ api/src/database.js
@@ -5,23 +5,11 @@ const user = process.env.MARIADB_USER
 const pwd = process.env.MARIADB_PASSWORD
 const db = process.env.MARIADB_DATABASE
 
-let sequelize
-
-if (process.env.NODE_ENV === 'test') {
-  sequelize = new Sequelize(db, user, pwd, {
-    host: '127.0.0.1', // Docker Service Name
-    dialect: 'mariadb',
-    logging: false, // Remove if SQL logs are wanted
-    protocol: 'tcp',
-    port: 3306,
-  })
-} else {
-  sequelize = new Sequelize(db, user, pwd, {
-    host: 'database', // Docker Service Name
-    dialect: 'mariadb',
-    logging: false, // Remove if SQL logs are wanted
-  })
-}
+sequelize = new Sequelize(db, user, pwd, {
+  host: 'database', // Docker Service Name
+  dialect: 'mariadb',
+  logging: false, // Remove if SQL logs are wanted
+})
 
 // Test connection to database
 async function testAuthenticate() {
