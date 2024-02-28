35b72321dd7d6f079fbc9114b1a3d3ddf62fb0a8
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2024-02-28 13:49:56 +0100

    use require in idex

--- api/test/index.js
+++ api/test/index.js
@@ -1,7 +1,6 @@
-// Vars
-import express from 'express'
-import dotenv from 'dotenv'
-import loader from './src/loaders/index'
+const express = require('express');
+const dotenv = require('dotenv');
+const loader = require('./src/loaders/index');
 
 const app = express()
 
@@ -9,14 +8,14 @@ loader(app);
 
 // Select the env file
 dotenv.config()
-const PORT = 5001
+const PORT = 5001;
 
 // Start server
-app.listen(port, err => {
+app.listen(PORT, err => {
   if (err) {
     console.log(err);
     return process.exit(1);
   }
 
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is running on ${PORT}`);
 });
