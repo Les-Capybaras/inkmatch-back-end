98f67937975a91aa2d77c2bdb6337390ece3e691
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2024-02-28 15:27:30 +0100

    update path

--- api/src/loaders/database.js
+++ api/src/loaders/database.js
@@ -1,4 +1,4 @@
-const User = require('./models/User')
+const User = require('../models/User')
 
 module.exports = async () => {
   console.log("database called");
@@ -6,10 +6,9 @@ module.exports = async () => {
   console.log(`[DEBUG] - Start Syncing`)
 
     try {
-      // await sequelize.sync({ force: true, alter: true });
       await User.sync()
       console.log('[DATABASE] - Synced database.')
-      app.emit("appStarted");
+      app.emit("databaseSynced");
     } catch (error) {
       console.error('[DATABASE] - Unable to sync database:', error)
     }
