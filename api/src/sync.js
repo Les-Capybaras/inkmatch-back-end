99bfdb8104800290811a8867f15e5d8ff8a7c747
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2024-02-28 14:15:49 +0100

    remove useless function

--- api/src/sync.js
+++ api/src/sync.js
@@ -2,16 +2,13 @@ module.exports = async () => {
   const User = require('./models/User')
   console.log(`[DEBUG] - Start Syncing`)
 
-  const syncDatabase = async () => {
-    try {
-      // await sequelize.sync({ force: true, alter: true });
-      await User.sync()
-      console.log('[DATABASE] - Synced database.')
-    } catch (error) {
-      console.error('[DATABASE] - Unable to sync database:', error)
-    }
+  try {
+    // await sequelize.sync({ force: true, alter: true });
+    await User.sync()
+    console.log('[DATABASE] - Synced database.')
+  } catch (error) {
+    console.error('[DATABASE] - Unable to sync database:', error)
   }
-  await syncDatabase()
 
   console.log(`[DEBUG] - Start Synced`)
 }
