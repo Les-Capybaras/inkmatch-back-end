95d9cd4ab8d5c82ec02d833f34ef98e46ed70d20
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2024-02-28 14:28:53 +0100

    use then

--- api/test/index.js
+++ api/test/index.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const loader = require('../src/loaders/index');
-
+const PORT = 5001;
 const app = express()
 
-loader(app);
+loader(app)
+.then(() => {
+  // Select the env file
+  dotenv.config()
 
-// Select the env file
-dotenv.config()
-const PORT = 5001;
+  // Start server
+  app.listen(PORT, err => {
+    if (err) {
+      console.log(err);
+      return process.exit(1);
+    }
 
-// Start server
-app.listen(PORT, err => {
-  if (err) {
-    console.log(err);
-    return process.exit(1);
-  }
+    console.log(`Server is running on ${PORT}`);
+  });
+}
 
-  console.log(`Server is running on ${PORT}`);
-});
+);
+
+
