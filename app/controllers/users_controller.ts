eb1984d3771efdc67247415080dfab5be083f0fa
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-03-28 14:18:48 +0100

    fix typecheck

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,7 +1,9 @@
 import { HttpContext } from '@adonisjs/core/http'
+import logger from '@adonisjs/core/services/logger'
 
 export default class UsersController {
   async index(ctx: HttpContext) {
+    logger.info(ctx.inspect())
     return [
       {
         id: 1,
