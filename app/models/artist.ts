56d70fa26a40d9223145048ebe528dd931247525
ismadev <ismadev.work@gmail.com>
2024-07-25 14:56:35 +0200

    hotfix - Artist model definition

--- app/models/artist.ts
+++ app/models/artist.ts
@@ -1,12 +1,62 @@
-import { column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import hash from '@adonisjs/core/services/hash'
 import Showcase from './showcase.js'
-import User from './user.js'
 import { LegalForm } from '../enums/legal_form.js'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import type { HasMany, HasOne } from '@adonisjs/lucid/types/relations'
 import Appointement from './appointement.js'
+import { compose } from '@adonisjs/core/helpers'
+import { DateTime } from 'luxon'
 
-export default class Artist extends User {
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class Artist extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column()
+  declare firstName: string | null
+
+  @column()
+  declare lastName: string | null
+
+  @column()
+  declare phoneNumber: string | null
+
+  @column()
+  declare address: string | null
+
+  @column()
+  declare city: string | null
+
+  @column()
+  declare zipCode: string | null
+
+  @column()
+  declare country: string | null
+
+  @column({ consume: (value) => !!value })
+  declare hasConfirmedEmail: boolean
+
+  @hasMany(() => Appointement)
+  declare appointements: HasMany<typeof Appointement>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
   @column()
   declare siret: string
 
@@ -18,9 +68,6 @@ export default class Artist extends User {
 
   @hasOne(() => Showcase)
   declare showcase: HasOne<typeof Showcase>
-
-  @hasMany(() => Appointement)
-  declare appointements: HasMany<typeof Appointement>
 
   static artistAccessTokens = DbAccessTokensProvider.forModel(Artist, {
     table: 'artists_access_tokens',
