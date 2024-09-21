6e4b6d8e520f263e4da3369e60da9683aa048b66
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-09-21 16:32:58 +0200

    INK-48: implement search in many properties

--- tests/functional/appointement/appointements.spec.ts
+++ tests/functional/appointement/appointements.spec.ts
@@ -119,7 +119,7 @@ test.group('As an artist, I should be able to take an appointement with a custom
       .post('/appointements/artist-to-client')
       .bearerToken(await loginAsArtist(client, 2))
       .json({
-        userId: 2,
+        userId: 1,
         date: '2024-11-11',
         description: 'A description',
       })
