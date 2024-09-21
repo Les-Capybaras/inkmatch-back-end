47c5060dac487e3aece0c77bde1dc577c2399ac8
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-09-21 17:00:36 +0200

    INK-48: implement search in many properties

--- tests/functional/appointement/appointements.spec.ts
+++ tests/functional/appointement/appointements.spec.ts
@@ -123,7 +123,7 @@ test.group('As an artist, I should be able to take an appointement with a custom
         date: '2024-11-11',
         description: 'A description',
       })
-    //console.log(response.body())
+    console.log(response.body())
     assert.equal(201, 201)
     //assert.equal(response.body().description, 'A description')
     //TODO : Fix this test
